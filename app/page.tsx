0330b2987a4812a7323a485fe2e64c022e40504c
Codeprince001 <urangyorkwisdom@gmail.com>
2025-07-23 18:18:10 +0100

    fix typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import HowItWorksSection from '@/components/Work';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
 
   return (
     <div className="min-h-screen flex flex-col">
